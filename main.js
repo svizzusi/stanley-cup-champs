b154ba01f0f4dc01b3c9249facfd832e375ad16a
Stephen Vizzusi <svizzusi13@gmail.com>
2023-06-15 12:10:41 -0700

    changed data.year issue

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ async function apiRequest(){
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('.h2').innerText = data.year
+        document.querySelector('.h2').innerText = "Year: " + data
     }catch(error){
         console.log(error)
     }
